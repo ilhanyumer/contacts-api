368bb2a9d55835a52e39a0539a20281fd64fe1ce
Ilhan Yumer <ilhan@ilhan.name>
2020-10-19 10:03:09 +0300

    Initial commit

---
+++ src/test/java/name/ilhan/contacts/ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package name.ilhan.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
