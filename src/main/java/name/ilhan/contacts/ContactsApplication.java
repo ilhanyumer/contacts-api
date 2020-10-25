1c708c6775c20297ffe91d36186bfb049c1d0863
İlhan Subaşı <ilhan@ilhan.name>
2020-10-25 17:03:29 +0300

    Use springfox for API documantation

--- src/main/java/name/ilhan/contacts/ContactsApplication.java
+++ src/main/java/name/ilhan/contacts/ContactsApplication.java
@@ -2,8 +2,10 @@ package name.ilhan.contacts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ContactsApplication {
 
 	public static void main(String[] args) {
