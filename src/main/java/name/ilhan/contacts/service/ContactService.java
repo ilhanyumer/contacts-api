a5c800af337ed7c8b8439d661f823702539a337a
Ilhan Yumer <ilhan@ilhan.name>
2020-11-01 12:58:13 +0300

    Update a contact partially

--- src/main/java/name/ilhan/contacts/service/ContactService.java
+++ src/main/java/name/ilhan/contacts/service/ContactService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContactService {
@@ -43,6 +44,19 @@ public class ContactService {
         contact.setName(contactInput.getName());
         contact.setSurname(contactInput.getSurname());
         contact.setId(id);
+        return contactsRepository.save(contact);
+    }
+
+    public Contact partialUpdateContact(Integer id, ContactInput contactInput) {
+        Optional<Contact> contactOptional = contactsRepository.findById(id);
+        Contact contact = contactOptional.get();
+        contact.setId(id);
+        if(contactInput.getName() != null) {
+            contact.setName(contactInput.getName());
+        }
+        if(contactInput.getSurname() != null) {
+            contact.setSurname(contactInput.getSurname());
+        }
         return contactsRepository.save(contact);
     }
 }
