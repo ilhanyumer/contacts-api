f5c055ee9bc98322b19c9a502e9518110b59af52
Ilhan Yumer <ilhan@ilhan.name>
2020-11-03 17:51:40 +0300

    Sort by id

--- src/main/java/name/ilhan/contacts/repository/ContactRepository.java
+++ src/main/java/name/ilhan/contacts/repository/ContactRepository.java
@@ -1,9 +1,18 @@
 package name.ilhan.contacts.repository;
 
 import name.ilhan.contacts.model.Contact;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContactRepository extends CrudRepository<Contact, Integer> {
+    @Override
+    default List<Contact> findAll() {
+        return findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
+
+    List<Contact> findAll(Sort id);
 }
